73c2aa4c2fe600c20749cf5311fd8698eaf5126d
roshansibi <roshansibi1234.com>
2024-06-12 15:10:30 +0530

    new path added

--- main.go
+++ main.go
@@ -9,7 +9,12 @@ func main() {
 		c.JSON(200, gin.H{
 			"message": "skill-map-home-page",
 		})
+		r.GET("/login", func(c *gin.Context) {
+			c.JSON(200, gin.H{
+				"message": "user-login-required",
+			})
 	})
 	r.Run() 
 }
 
+cd
